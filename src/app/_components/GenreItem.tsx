dec90d52837662bbf65ea51a1b1c89df19dde9ae
25LP1467 25LP1467 <25LP1467@pinecones-iMac.local>
2026-02-02 12:30:31 +0800

    hehe

--- src/app/_components/GenreItem.tsx
+++ src/app/_components/GenreItem.tsx
@@ -1,23 +1,23 @@
-"use client";
+// "use client";
 
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { ChevronRight } from "lucide-react";
+// import { Badge } from "@/components/ui/badge";
+// import { Button } from "@/components/ui/button";
+// import { ChevronRight } from "lucide-react";
 
-type GenreItemProps = {
-  id: number;
-  name: string;
-};
+// type GenreItemProps = {
+//   id: number;
+//   name: string;
+// };
 
-export const GenreItem = (props: GenreItemProps) => {
-  return (
-    <Badge
-      className="w-fit h-5 flex items-center gap-2 cursor-pointer hover:bg-black hover:text-white "
-      variant={"outline"}
-      onClick={() => handleClickGenre(props.id)}
-    >
-      {props.name}
-      <ChevronRight strokeWidth={1} />
-    </Badge>
-  );
-};
+// export const GenreItem = (props: GenreItemProps) => {
+//   return (
+//     <Badge
+//       className="w-fit h-5 flex items-center gap-2 cursor-pointer hover:bg-black hover:text-white "
+//       variant={"outline"}
+//       onClick={() => handleClickGenre(props.id)}
+//     >
+//       {props.name}
+//       <ChevronRight strokeWidth={1} />
+//     </Badge>
+//   );
+// };
